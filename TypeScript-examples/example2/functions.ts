5de227380c9f639c6e247be20cd1ab9b8339f0a7
youssef baddi <baddi.y@ucd.ac.com>
2018-04-28 17:39:07 +0100

    update functions examples

--- TypeScript-examples/example2/functions.ts
+++ TypeScript-examples/example2/functions.ts
@@ -8,3 +8,21 @@ function hello(name: string): number {
 }
 
 console.log(hello('test'));
+
+
+function setName(name: string): void {
+    console.log('void function');
+}
+
+function isPaire(x: number): boolean {
+    return x % 2 === 0;
+}
+
+function isPaire2(nombre: number | string): boolean {
+    if(typeof  nombre !== 'number'){
+        nombre = parseInt(<string>nombre,10);
+    }
+    return <number>nombre % 2 === 0;
+}
+
+console.log(isPaire2('2'));
