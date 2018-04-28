1866c1769fc4baa380790266fa0600aafc01e2e1
youssef baddi <baddi.y@ucd.ac.com>
2018-04-28 19:19:33 +0100

    update template string examples

---
+++ TypeScript-examples/example1/templateStrings.ts
@@ -0,0 +1,15 @@
+var firstName = "Nate";
+var lastName = "Murray";
+// interpolate a string
+var greeting = `Hello ${firstName} ${lastName}`;
+console.log(greeting);
+
+
+
+var age:number = 20
+var firstName: string = 'baddi'
+var secondName: string = 'youssef'
+var sentence: string = 
+`Hello, my name is ${ firstName } ${ secondName }.
+I'll be ${ age + 1 } years old next month.`
+console.log(sentence);
